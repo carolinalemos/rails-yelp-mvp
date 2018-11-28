604d32a05495b1835f79cc5d14c4182a901865ac
Carolina Lemos <carolinaperestrelodelemos@gmail.com>
2018-11-28 14:44:31 +0000

    Created Restaurant and Review models and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    category: 'belgian'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '01 333 5665',
+    category: 'italian'
+  },
+  {
+    name: 'La Cuisine',
+    address: 'Rua da Moeda',
+    phone_number: '213 45 89',
+    category: 'french'
+  },
+  {
+    name: 'Best Sushi',
+    address: 'Cais do Sodre',
+    phone_number: '241 333 5665',
+    category: 'japanese'
+  },
+  {
+    name: 'Jamie Oliver',
+    address: 'Principe Real',
+    phone_number: '098 765 443',
+    category: 'italian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
